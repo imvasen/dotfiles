dafc60d5415be65f686875132e982e58bba4ada3
Héctor J. Vásquez <me@vasen.dev>
2024-08-15 12:37:17 -0600

    feat: add brew bundle

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ set -euxo pipefail
 
 echo "Running installation"
 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew bundle
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Zsh
