1d3e3ee1ea1040a0d680bec03d018c056a02d7d5
Héctor J. Vásquez <me@vasen.dev>
2024-12-22 23:13:18 -0600

    feat: add lsp configs

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,12 @@
 require('config.lazy')
 require('lualine').setup()
+require('config.lsp')
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+-- True colour support
+vim.cmd.colorscheme('tokyonight-night')
+
 -- Neovim init.lua
 -- nvim-tree recommends disabling netrw, VIM's built-in file explorer
 vim.g.loaded_netrw = 1
@@ -8,17 +15,10 @@ vim.g.loaded_netrwPlugin = 1
 -- Remove the white status bar below
 vim.o.laststatus = 0
 
--- True colour support
-vim.o.termguicolors = true
-
 -- .vimrc stuff
 vim.cmd 'syntax on'
-vim.cmd [[ colorscheme tokyonight-night ]]
 vim.cmd 'set directory^=$HOME/.config/nvim/tmp//'
 vim.cmd 'nmap <Bs> <S-x>'
-vim.cmd 'set colorcolumn=80'
-vim.cmd 'highlight ColorColumn ctermbg=0 guibg=orange'
-
 vim.o.cursorline = true
 
 local builtin = require('telescope.builtin')
@@ -41,16 +41,22 @@ vim.keymap.set('n', '<leader>w', ':w<CR>', { silent = true })
 vim.keymap.set('n', '<leader>U', ':Lazy sync<CR>', { silent = true })
 
 vim.opt.tabstop = 2
+
+-- Reserve a space in gutter to avoid layout shift
+vim.opt.signcolumn = 'number'
 vim.opt.number = true
+vim.opt.list = true
+vim.opt.listchars = { tab = '▸ ', trail = '·', nbsp = '␣' }
+vim.opt.relativenumber = true
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.bo.softtabstop = 2
+vim.opt.colorcolumn = '80,120'
+vim.cmd 'highlight ColorColumn ctermbg=130 guibg=#444444'
+vim.opt.wrap = false
 
 -- vim tree
 -- disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-
--- optionally enable 24-bit colour
-vim.opt.termguicolors = true
 
---
+++ nvim/lua/plugins/mason-lspconfig.lua
@@ -0,0 +1,13 @@
+return {
+  'williamboman/mason-lspconfig.nvim',
+  config = function()
+    require('mason').setup({})
+    require('mason-lspconfig').setup({
+      handlers = {
+        function (server_name)
+          require('lspconfig')[server_name].setup({})
+        end,
+      },
+    })
+  end
+}
